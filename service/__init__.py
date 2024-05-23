d35b6f03ed24c31a7c9871194fdc8e062e90ca91
Mohsen-Gayem-Ahvaz <153442163+Mohsen-Gayem-Ahvaz@users.noreply.github.com>
2024-05-23 20:25:56 +0330

    c-3

--- service/__init__.py
+++ service/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 
+# App Init
 service = Flask(import_name=__name__, template_folder="templates", static_folder="static")
-
 
 @service.route("/")
 def root():
