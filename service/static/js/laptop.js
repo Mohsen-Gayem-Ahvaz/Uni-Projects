670c980a0e0980201fec0be2fb5a544b519f3687
Mohsen-Gayem-Ahvaz <mohsen.g.a.2001@gmail.com>
2024-05-25 02:14:02 +0330

    c-9

--- service/static/js/laptop.js
+++ service/static/js/laptop.js
@@ -1,2 +1,14 @@
 console.log("Laptop Script Implemented");
 
+async function download_csv(url) {
+    const resp = await fetch(url)
+        .then(resp => resp.text())
+        .then(value => Papa.parse(value))
+        .catch(err => console.log(err));
+    return resp
+}
+
+let laptop_data = download_csv("http://127.0.0.1:5050/static/dataset/laptop_info.csv");
+laptop_data.then(value => {
+    console.log(value.data[1078][0])
+})
