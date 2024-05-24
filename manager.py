d8c6e53f5aa4df280538c7e1a2de11d2f553cf2e
Mohsen-Gayem-Ahvaz <mohsen.g.a.2001@gmail.com>
2024-05-25 00:41:58 +0330

    c-7

--- manager.py
+++
@@ -1,6 +0,0 @@
-from service import service
-
-if __name__ == '__main__':
-    service.run(
-        host="0.0.0.0", port=5050
-    )
--- service/__init__.py
+++ service/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
 
-# App Init
 service = Flask(import_name=__name__, template_folder="templates", static_folder="static")
 
 @service.route("/")
@@ -9,3 +8,7 @@ def root():
     return render_template("index.html")
 
 
+if __name__ == '__main__':
+    service.run(
+        host="0.0.0.0", port=5050
+    )
