c91b40302a9f872fd59523efce871f4fd77ed073
Mohsen-Gayem-Ahvaz <mohsen.g.a.2001@gmail.com>
2024-05-25 02:38:35 +0330

    c-11

--- service/static/js/laptop.js
+++ service/static/js/laptop.js
@@ -1,24 +1,21 @@
 console.log("Laptop Script Implemented");
 
+const laptopInfoUrls = [
+    "http://mohsenga.pythonanywhere.com/static/dataset/laptop_info.csv",
+    "http://127.0.0.1:5050/static/dataset/laptop_info.csv"
+]
+
 async function download_csv(url) {
-    const reqOptions = {
-        headers: new Headers({
-            type: "GET",
-            dataType: 'csv',
-            xhrFields: {
-                withCredentials: true
-            },
-            crossDomain: true,
-        })
-    }
+    
     const resp = await fetch(url)
         .then(resp => resp.text())
         .then(value => Papa.parse(value))
         .catch(err => console.log(err));
+
     return resp
 }
 
-let laptop_data = download_csv("http://mohsenga.pythonanywhere.com/static/dataset/laptop_info.csv");
+let laptop_data = download_csv(laptopInfoUrls[1]);
 laptop_data.then(value => {
-    console.log(value.data[1078][0])
+    console.log(value.data)
 })
