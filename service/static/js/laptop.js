bdeac4e46f8419c9cab5823bd22aca5f18b4ab60
Mohsen-Gayem-Ahvaz <mohsen.g.a.2001@gmail.com>
2024-05-25 04:33:10 +0330

    c-11

--- service/static/js/laptop.js
+++ service/static/js/laptop.js
@@ -1,21 +1,36 @@
-console.log("Laptop Script Implemented");
+
 
 const laptopInfoUrls = [
     "http://mohsenga.pythonanywhere.com/static/dataset/laptop_info.csv",
     "http://127.0.0.1:5050/static/dataset/laptop_info.csv"
-]
+];
+var laptopTb;
+var laptopData = [];
 
 async function download_csv(url) {
-
     const resp = await fetch(url)
         .then(resp => resp.text())
         .then(value => Papa.parse(value))
         .catch(err => console.log(err));
 
-    return resp
+    return resp;
 }
 
-let laptop_data = download_csv(laptopInfoUrls[0]);
-laptop_data.then(value => {
-    console.log(value.data)
-})
+async function reloadData(){
+    let csvResp = download_csv(laptopInfoUrls[0]);
+    await csvResp.then((value) => {
+        laptopTb = document.getElementById("laptops-tbody");
+        laptopData = value.data;
+        for (let index = 0; index <= laptopData.length; index+=1){
+            let currRow = laptopTb.insertRow(index)
+            currRow.insertCell(0).innerHTML = index;
+            for (let cell = 0; cell <= 5;cell+=1){
+                currRow.insertCell(cell+1).innerHTML = laptopData[index][cell];
+            }
+        }
+    })
+}
+
+window.addEventListener("DOMContentLoaded", function() {
+    reloadData()
+}, false);
